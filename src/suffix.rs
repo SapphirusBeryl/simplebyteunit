da54d9b7389a3a540e698f224f14dc7212c5429e
Xavier Moffett <sapphirus@azorium.net>
2025-07-19 18:42:37 -0400

    chore: Add `LICENSE` file and add SPDX header

--- src/suffix.rs
+++ src/suffix.rs
@@ -2,6 +2,7 @@
  * SimpleByteUnit
  *
  * Copyright (C) 2023-2025 Xavier Moffett <sapphirus@azorium.net>
+ * SPDX-License-Identifier: Apache-2.0
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
